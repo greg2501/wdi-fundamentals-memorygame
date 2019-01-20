cbe783c8e169e78886aad4258a0fffc28ee90e17
greg <greg.poulos81@gmail.com>
2019-01-20 08:22:09 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,3 @@
-console.log("Up and running!");
-
-//var cardOne = "queen";
-//var cardTwo = "queen";
-//var cardThree = "king";
-//var cardFour = "king";
-
-
-
-//alert('Hello, friends.');
-
 var cards = [
 {
 rank: "queen",
@@ -36,20 +25,30 @@ var cardsInPlay = []
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
-}}
+	alert("Sorry, try again.");
+}};
 
-var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId].rank)
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id')
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {checkForMatch();
 		console.log("User flipped " + cards[cardId].rank);
 		console.log(cards[cardId].cardImage);
 		console.log(cards[cardId].suit);
 
-}}
+}};
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+    }
+};
 
-flipCard(0)
-flipCard(2)
+createBoard();
